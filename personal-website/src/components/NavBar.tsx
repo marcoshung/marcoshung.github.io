fd54c4d13481fd638fc37c660097ea9c5d8522be
Marcos hung <mhung927@gmail.com>
2021-08-24 20:20:51 -0700

    react typescript fixes

--- personal-website/src/components/NavBar.tsx
+++ personal-website/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './NavBar.css'
 import { Link } from 'react-router-dom'
 export const NavBar = () =>{
--- personal-website/src/components/pages/Home.tsx
+++ personal-website/src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { TypeWriter, NavBar } from '../'
 import { motion } from 'framer-motion'
 export const Home = () => {
