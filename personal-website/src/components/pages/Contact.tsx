43cee9e51892082ad127d3a67b586d56e4f51287
marcos hung <mhung927@gmail.com>
2021-12-28 02:42:58 -0800

    updated contact page

--- personal-website/src/components/pages/Contact.tsx
+++ personal-website/src/components/pages/Contact.tsx
@@ -4,36 +4,76 @@ import React, { useState } from 'react';
 import './Contact.css'
 import linkedin from '../../images/linkedIn.png';
 import github from '../../images/github.png';
+import { useForm, ValidationError } from '@formspree/react';
+
 export const Contact= ()=> {
-    const [phoneMessage, setPhoneMessage] = useState('')
-    
+    const [state, handleSubmit] = useForm("xayvnqky");
+    const [name, SetName] = useState('')
     return (
-        <>  
-            <div>
-            <h3>Send Me a Text!</h3>
-                <textarea 
-                    className="phoneMessage"
-                    onChange={(event) => setPhoneMessage(event?.target.value)}
-                    placeholder="Write me a message!"
-                    style={{resize:"both"}}
-                    rows={4}
-                    cols={64}
-                />
-                
-            </div>
-            <ChakraProvider>
-                <Button 
-                    colorScheme="blue" 
-                    size="lg"
-                    onClick={() =>Message(phoneMessage)}
-                >
-                    Send Message
-                    </Button>
-            </ChakraProvider>
+        <div className="contact">  
+            <h3>Contact</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="contact-header">
+                    <div className="contact-content" style={{"marginRight": "8px"}}>
+                        <label htmlFor="name">
+                            Name
+                        </label>
+                        <br></br>
+                        <input
+                            id="name"
+                            name="name"
+                            onChange={(event) => SetName(event?.target.value)}
+                        />
+                    </div>
+                    <div className="contact-content" style={{"marginLeft": "8px"}}>
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                        <br></br>
+                        <input
+                            id="email"
+                            type="email" 
+                            name="email"
+                        />
+                        <ValidationError 
+                            prefix="Email" 
+                            field="email"
+                            errors={state.errors}
+                        />
+                    </div>
+                </div>
+                <div className="contact-message">
+                    <label htmlFor="message">
+                        Message
+                    </label>
+                    <br></br>
+                    <textarea
+                        id="message"
+                        name="message"
+                        style={{"fontSize":"medium"}}
+                        rows={5}
+                    />
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                </div>
+                    <ChakraProvider>
+                        <Button 
+                            colorScheme="blue" 
+                            size="lg"
+                            type="submit"
+                        >
+                            Send Message
+                            </Button>
+                    </ChakraProvider>
+                </form>
+
             <div className="contact_bar">
                 <a href="https://www.linkedin.com/in/mhung927/" className="icon"><img src={linkedin}/> </a>
                 <a href="https://github.com/marcoshung" className="icon"><img id ={"github"} src={github}/> </a>
             </div>
-        </>
+        </div>
     )
 }
